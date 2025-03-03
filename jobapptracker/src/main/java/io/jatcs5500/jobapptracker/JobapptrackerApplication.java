5fcf72df273a60942a3c25b2291e845d38d5e964
Aryan Kakadia <aryan.kakadia10@gmail.com>
2025-03-03 13:18:45 -0800

    Init project

---
+++ jobapptracker/src/main/java/io/jatcs5500/jobapptracker/JobapptrackerApplication.java
@@ -0,0 +1,13 @@
+package io.jatcs5500.jobapptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapptrackerApplication.class, args);
+	}
+
+}
---
+++ jobapptracker/src/test/java/io/jatcs5500/jobapptracker/JobapptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jatcs5500.jobapptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
