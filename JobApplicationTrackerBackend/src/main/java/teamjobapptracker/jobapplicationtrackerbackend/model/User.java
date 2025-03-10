d9ad9628ff7ae8274682319dc3cd9aec43f7824b
Aryan Kakadia <aryan.kakadia10@gmail.com>
2025-03-10 12:46:13 -0700

    Add User model

--- JobApplicationTrackerBackend/src/main/java/teamjobapptracker/jobapplicationtrackerbackend/model/User.java
+++ JobApplicationTrackerBackend/src/main/java/teamjobapptracker/jobapplicationtrackerbackend/model/User.java
@@ -1,12 +1,46 @@
 package teamjobapptracker.jobapplicationtrackerbackend.model;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-public class User implements Serializable {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-  private long userID;
-  private String userName;
-  private String email;
+@Entity
+@Table(name = "users")
+@Data
+public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false)
+    private String role = "USER"; // Default role
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobApplication> applications = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
