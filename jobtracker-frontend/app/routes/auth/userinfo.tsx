5b785f3f2f6b0322cadafbc2529d680d9ea75360
honzie12 <yu.hongz@northeastern.edu>
2025-03-24 21:55:09 -0700

    add education to user info

--- jobtracker-frontend/app/routes/auth/userinfo.tsx
+++ jobtracker-frontend/app/routes/auth/userinfo.tsx
@@ -2,13 +2,27 @@ import { useRef, useState } from 'react';
 import blankAvatar from '/images/blank-avatar.png';
 import uploadLogo from '/images/avatar-upload.png';
 import OptionsSelector from '~/components/OptionsSelector';
+import { useNavigate } from 'react-router';
 
 export default function UserInfo() {
     const [avatar, setAvatar] = useState(blankAvatar);
     const [gender, setGender] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [degree, setDegree] = useState('');
+    const [field, setField] = useState('');
+
+    // implement a function that if it's not a new user(decide based on the user account 
+    // created time, less than 30 minutes), 
+    // then navigate to dashboard. 
+    // The user info collection can only be done there.
+
+    const navigate = useNavigate();
+
     const avatarInputRef = useRef<HTMLInputElement>(null);
 
     const genderOptions = ['Female', 'Male', 'Non-binary', 'Others', 'Prefer not to say'];
+    const degreeOptions = ["High School Diploma", "Associate's Degree", "Bachelor's Degree", 
+        "Master's Degree", "Doctoral Degree", "Certificate/Diploma", "No Formal Education", "Other"];
 
     const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -24,9 +38,14 @@ export default function UserInfo() {
         avatarInputRef.current?.click();
     }
 
+    // unimplemented
+    const handleContinueClick = () => {
+
+    };
+
     return (
         <div className='mt-[16vh]'>
-            <form className='flex flex-col items-center gap-5'>
+            <form className='flex flex-col items-center gap-8'>
                 <div>
                     <div onClick={handleAvatarClick} className='flex relative w-fit cursor-pointer'>
                         <img src={avatar} alt="avatar" className='h-20'/>
@@ -50,11 +69,25 @@ export default function UserInfo() {
                     <div>
                         <label htmlFor="birthday">
                             Date of Birth:
-                            <input type="date" className='ml-2'/>
+                            <input type="date" className='ml-2' onChange={(e) => setBirthday(e.target.value)}/>
                         </label>
                     </div>
+                    <div className='flex flex-row'>
+                        <label htmlFor="education">
+                            Education: 
+                        </label>
+                        <div className='flex flex-col gap-2'>
+                            <select name="education-degree" id="education-degree" className='ml-2' onChange={(e) => setDegree(e.target.value)}>
+                                <option disabled selected>---Select a degree---</option>
+                                {degreeOptions.map((degree, index) => {
+                                    return <option key={index} value={degree}>{degree}</option>
+                                })}
+                            </select>
+                            <input type="text" placeholder='Field of study' value={field} onChange={(e) => setField(e.target.value)} className='border-2 rounded-[10px] pl-2'/>
+                        </div>
+                    </div>
                 </div>
-                <button type='submit' className='border-2 bg-[#FFC457] rounded-2xl px-8'>Continue</button>
+                <button type='submit' className='border-2 bg-[#FFC457] rounded-2xl px-8 cursor-pointer'>Continue</button>
             </form>
             
         </div>
