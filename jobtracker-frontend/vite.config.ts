2ffb48ff84cd68e235b343bcc98b3b84d060e167
honzie12 <yu.hongz@northeastern.edu>
2025-04-17 14:44:47 -0700

    reconstruct the front end

--- jobtracker-frontend/vite.config.ts
+++ jobtracker-frontend/vite.config.ts
@@ -1,37 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-core': ['react', 'react-dom'],
-          'router': ['@react-router/node', '@react-router/serve', 'react-router', '@react-router/dev', 'react-router-devtools'],
-          'redux': ['@reduxjs/toolkit', 'react-redux'],
-          'ui-components': ['react-hot-toast', 'isbot'],
-          'network': ['axios'],
-          'styling': ['tailwindcss', '@tailwindcss/vite'],
-          'dev-tools': [
-            '@types/node', 
-            '@types/react', 
-            '@types/react-dom', 
-            'typescript', 
-            'vite-tsconfig-paths']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router', '@reduxjs/toolkit', 'react-redux', 'axios'],
-  }
-});
+  plugins: [react()],
+})
